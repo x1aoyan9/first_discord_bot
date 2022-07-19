71f833d51400bc913492cc52571026346d3563fe
Sarah Lynn <98367848+x1aoyan9@users.noreply.github.com>
2022-07-19 16:06:20 -0500

    Create sad_words.py

---
+++ sad_words.py
@@ -0,0 +1,22 @@
+# Dictionary of sad words
+# On a different file to make it easier to see what's currently added
+# Makes it easier to add and remove from the dictionary
+
+# Keep wondering if there's a simpler way to do this?
+# Guess I know what to research
+
+sad_words = ["sad","Sad",
+             "depressed","Depressed",
+             "unhappy","Unhappy",
+             "sucks","Sucks",
+             "miserable","Miserable",
+             "depressing","Depressing",
+             "horrible","Horrible",
+             "crappy","Crappy",
+             "crap","Crap",
+             "shitty","Shitty",
+             "shit","Shit",
+             "worst","Worst",
+             "send help","Send Help",
+             "rip","RIP"
+            ]
